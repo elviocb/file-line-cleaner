17fc1630382055da29a77128be69621f50dfc6b1
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-07-29 21:56:38 -0300

    Read stdin and parse the lines into an array

--- server.js
+++ server.js
@@ -5,13 +5,34 @@ var fs = require('fs');
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', function(chunk) {
-    lines = chunk.split("\n");
+var allLines = []
 
-    console.log(lines);
+process.stdin.on('data', function(chunk) {
+    var lines = chunk.split("\n");
+    allLines = allLines.concat(lines);
+    // console.log(allLines);
 });
 
 process.stdin.on('end', function() {
+    console.log(__dirname + '/dps.txt');
+  //   console.log(fs.readFileSync(__dirname + "/dps", function(err, file){
+  //     if(err) { console.log('error', err) }
+  //     console.log(file);
+  // }));
+    fs.readFile(__dirname + '/dps.txt', function (err, data) {
+      if (err) throw err;
+      data = data + '';
+      var ids = data.split('\n');
+      ids.pop();
+      console.log(ids);
+    });
+    
+    console.log(process.argv);
+
+    // allLines = allLines.filter(function(line) 
+    //   return ;
+    // });
+    console.log(allLines.length);
     console.log('ennnnnddddd');
 });
 
